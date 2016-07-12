c92084eef08fc47c1ed7bad1e7b9b88b2d25f2ce
Nate Clark <natec425@gmail.com>
2016-07-12 08:32:03 -0500

    Add get_order stub

--- btc.py
+++ btc.py
@@ -14,3 +14,17 @@ def load_menu():
       {'name': 'chips', 'price': 1.50} ]
     """
     # YOUR CODE HERE
+
+def get_order(menu):
+    """ Menu -> Order
+
+    Returns the order from the user.
+    An Order is a list of 2-element tuples.
+    The first element is a MenuItem.
+    The second element is how many of that item were purchased.
+
+    Example Order:
+    [ ({'name': 'coke', 'price': .93}, 3),
+      ({'name': 'chips', 'price': 1.50}, 1)]
+    """
+    # YOUR CODE HERE
