b7990f04b17e3b47fd0d0fdd4c22393fa173186a
Nate Clark <natec425@gmail.com>
2016-07-12 08:26:29 -0500

    Add load_menu stub

---
+++ btc.py
@@ -0,0 +1,15 @@
+MENU_PATH = 'menu.txt'
+
+def load_menu():
+    """ () -> Menu
+
+    Loads the menu data stored at `MENU_PATH` and returns
+    the constructed menu.
+
+    The Menu is a list of dictionaries.
+
+    Example Menu:
+    [ {'name': 'coke', 'price': .93},
+      {'name': 'chips', 'price': 1.50} ]
+    """
+    # YOUR CODE HERE
